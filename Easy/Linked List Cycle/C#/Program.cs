da4568856f12d8a4498edc2ae3a55c9bd55efb96
Stanley Sandjo <r0878901@student.thomasmore.be>
2025-11-14 19:43:48 +0100

    Improved ListNode class - Solved - C# - Linked List Cycle

--- Easy/Linked List Cycle/C#/Program.cs
+++ Easy/Linked List Cycle/C#/Program.cs
@@ -3,8 +3,7 @@
 var task = new Solution();
 
 var node = new ListNode(-4);
-var head = new ListNode(3)
-.Add(2).Add(0).Add(node);
+var head = new ListNode([3,2,0]).Add(node);
 node.Add(head.next);
 
 var result = task.HasCycleDisruptive(head);
